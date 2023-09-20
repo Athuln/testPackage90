83aa5d18e965102c07e56ec9654f94440b887775
Athuln <athul.n@neoito.com>
2023-09-20 16:24:03 +0530

    feat: Initial commit

---
+++ testPackage90Pack/testFunction2/index.js
@@ -0,0 +1,26 @@
+
+/**
+ * @swagger
+ * /testfunction2:
+ *   post:
+ *     summary: Retrieve a list of JSONPlaceholder users for test function 2
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+*/
+const handler = async (event) => {
+
+  const {req, res} = event
+  
+  // health check
+  if (req.params.health === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+    return
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Happy Hacking`}))
+  res.end()
+  
+}
+
+export default handler
